73c319fb48ee4c745e11deb5170e1c7e7a0a95da
VitaHub <hubenko_vitalii@mail.ua>
2016-05-16 00:05:29 +0300

    birthdate starts from 1960

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  validates :login,  presence: true
+  validates :login,  presence: true, uniqueness: { case_sensitive: false }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
