d8f5dc76cb17ea23f1db66d81696a2a39b7eef79
VitaHub <hubenko_vitalii@mail.ua>
2016-05-16 04:18:29 +0300

    Russian language support added

--- Gemfile
+++ Gemfile
@@ -13,7 +13,9 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise'
+gem 'devise-i18n'
 gem 'bower-rails'
+gem 'russian', '~> 0.6.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/assets/javascripts/style.js
@@ -0,0 +1,19 @@
+var main = function(){
+	$( ".field input" ).addClass( "form-control" );
+
+	$( "#user_remember_me" ).removeClass( "form-control" );
+
+	$( ".field select" ).addClass( "form-control" );
+
+	$( ".actions" ).addClass( "text-center" );
+
+	$( ".actions input" ).addClass( "btn btn-primary" );
+
+	$( ".button_to input" ).addClass( "btn btn-primary btn-sm" );
+
+	$( ".button_to" ).addClass( "text-center" );
+
+};
+
+$(document).ready(main);
+$(document).on('page:load', main);
